f74ef50aeaba666d69b01902453025a29ecaf50b
patrikis-angelos <angelosp07@gmail.com>
2023-02-25 15:47:04 +0200

    Configure redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import './assets/index.css';
+
+import App from './App';
+import initialState from './redux/initialState';
+import rootReducer from './redux/reducers/rootReducer';
+
+const store = configureStore({ reducer: rootReducer, preloadedState: initialState });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
