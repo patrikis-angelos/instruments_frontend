6ba00d46ecd6bc46ff09e70cc809223373342565
patrikis-angelos <angelosp07@gmail.com>
2023-03-11 17:21:50 +0200

    Create header bar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,18 +1,19 @@
 import '../../assets/css/header.css';
+import { HiBars3 } from 'react-icons/hi2';
 
 function Header() {
   return (
     <header>
-      <nav className="flex space-between bg-color-secondary color-white nav-font">
+      <nav className="flex space-between bg-color-secondary color-white nav-font p-t-5 p-b-5 p-l-50 p-r-20">
         <div>
-          <div>Instrumentalist</div>
+          <a href="/">Instrumentalist</a>
         </div>
         <div className="flex">
-          <div>Keyboard</div>
-          <div>String</div>
-          <div>Brass</div>
-          <div>Woodwind</div>
-          <div><i className="" /></div>
+          <a href="/" className="m-r-20">Keyboard</a>
+          <a href="/" className="m-r-20">String</a>
+          <a href="/" className="m-r-20">Brass</a>
+          <a href="/" className="m-r-20">Woodwind</a>
+          <div className="flex flex-dir-col center color-orange"><HiBars3 /></div>
         </div>
       </nav>
     </header>
